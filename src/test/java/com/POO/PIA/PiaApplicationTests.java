aa139b2ffcb2118e4d417c64eb7c89b57d876e06
Rebeca <rebecasanchez2513@gmail.com>
2026-05-07 21:33:10 -0600

    first commit!

---
+++ src/test/java/com/POO/PIA/PiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.POO.PIA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
